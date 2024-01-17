2b28275b3c8313e28d6ab38ed374162d19c6c3fc
Dongxiaohao <1451361390@qq.com>
2024-01-17 17:21:35 +0800

    deployed

---
+++ src/main/java/top/dongxiaohao/validatap12status/ValidataP12StatusApplication.java
@@ -0,0 +1,13 @@
+package top.dongxiaohao.validatap12status;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidataP12StatusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidataP12StatusApplication.class, args);
+    }
+
+}
